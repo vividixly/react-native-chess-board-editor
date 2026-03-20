b9400bd1df4cae5be3c5e17aac272078e088f8fb
Denys Olofinskyi <denis.olofinskyi@gmail.com>
2026-03-20 13:47:36 +0100

    fix: clear library

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,7 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from '@vividixly/react-native-chess-board-editor';
-
-const result = multiply(3, 7);
+import { View, StyleSheet } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
