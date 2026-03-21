3c93495dfb3e9163271065f4aea02529406101a3
Denys Olofinskyi <denis.olofinskyi@gmail.com>
2026-03-21 14:53:07 +0100

    feat: update square feature

--- src/square/components/SvgSquare.tsx
+++ src/square/components/SvgSquare.tsx
@@ -1,7 +1,7 @@
-import type { SquareProps } from '../types/square.types';
+import { type SvgSquareProps } from '../types/svgSquare.types';
 import Svg, { Rect } from 'react-native-svg';
 
-export default function Square({ size, color }: SquareProps) {
+export default function SvgSquare({ size, color }: SvgSquareProps) {
   return (
     <Svg width={size} height={size}>
       <Rect width={size} height={size} fill={color} />
