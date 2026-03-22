528e889a6ba2f2716f28071dd12ce2e4f83e6c7e
Denys Olofinskyi <denis.olofinskyi@gmail.com>
2026-03-22 14:25:25 +0100

    feat: update player feature

--- src/player/index.tsx
+++ src/player/index.tsx
@@ -1,2 +1,2 @@
-export { type Player } from './types/player.types';
-export { type PlayerSet } from './types/playerSet.types';
+export type { Player } from './types/player.types';
+export type { PlayerSet } from './types/playerSet.types';
