bc50e09e2d970b7b94cfd8c252026d80240b54d2
Denys Olofinskyi <denis.olofinskyi@gmail.com>
2026-03-25 18:58:09 +0100

    feat: create piece set chess variant

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
 export { type Player, type PlayerSet, usePlayerSet } from './player';
 export { type Square, type SquareSet, useSquareSet } from './square';
-export { type Piece, type PieceSet, usePieceSet } from './piece';
+export { type Piece, type PieceSet, usePieceSet, PieceSets } from './piece';
--- src/piece/index.tsx
+++ src/piece/index.tsx
@@ -1,3 +1,4 @@
 export type { Piece } from './types/piece.types';
 export type { PieceSet } from './types/pieceSet.types';
 export { usePieceSet } from './hooks/usePieceSet';
+export * as PieceSets from './variants';
