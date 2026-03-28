3789eee97a084a28a6de354c3569be9d9ec8c367
Denys Olofinskyi <denis.olofinskyi@gmail.com>
2026-03-28 12:35:38 +0100

    feat: create 2 square set variants

---
+++ src/square/sets/variants/classic.ts
@@ -0,0 +1,9 @@
+import type { SquareSet } from '../../types/squareSet.types';
+
+export const classic: SquareSet = {
+  squares: {
+    light: '#f0d9b5',
+    dark: '#b58863',
+  },
+  name: 'classic',
+};
