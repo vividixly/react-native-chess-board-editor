149df7839ac4a5abd47738dba9f51f371af1c7b2
Denys Olofinskyi <denis.olofinskyi@gmail.com>
2026-03-29 15:01:39 +0200

    feat: create geometry feature

---
+++ src/geometry/index.tsx
@@ -0,0 +1,5 @@
+export type { Vector2D } from './types/vector.types';
+export type { Array2D } from './types/array.types';
+
+export * as Vector2DUtils from './utils/2d/vector.utils';
+export * as Array2DUtils from './utils/2d/array.utils';
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
 export { type Player, type PlayerSet, PlayerSets } from './player';
 export { type Square, type SquareSet, SquareSets } from './square';
 export { type Piece, type PieceSet, PieceSets } from './piece';
+export {
+  type Vector2D,
+  Vector2DUtils,
+  type Array2D,
+  Array2DUtils,
+} from './geometry';
